891c27ce5e6f9eda88c89acdbee1ae41ac235790
Bit Maximum <cabamax@yandex.ru>
2025-03-31 22:31:15 +1000

    Add prototype of Group Chart for Lab 4

---
+++ src/chart/Chart.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import GroupGrid from "./components/GroupGrid";
+
+function List() {
+    return (
+        <div>
+            <Navbar active="2"/>
+            <GroupGrid/>
+        </div>
+    );
+}
+
+export default List;
